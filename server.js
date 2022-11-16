645b42a659053d6a0404f3ca078e8485bc044975
aaronjohnston4 <aaron.johnston4@gmail.com>
2022-11-16 16:53:52 -0600

    next commit

--- server.js
+++ server.js
@@ -2,6 +2,25 @@
 // DEPENDENCIES
 ////////////////////////////////
 
+// ... the rest of our other dependencies
+
+const cors = require("cors")
+const morgan = require("morgan")
+
+///////////////////////////////
+// MIDDLEWARE
+////////////////////////////////
+app.use(express.json()); // parse json bodies - this will run before our request accesses the people router¸
+app.use(cors()); // to prevent cors errors, open access to all origins
+app.use(morgan("dev")); // logging for development
+// all requests for endpoints that begin with '/people'
+app.use('/people', peopleController)
+
+
+// import people router
+const peopleController = require('./controllers/people-controller')
+
+
 // initialize .env variables
 require("dotenv").config();
 
@@ -13,14 +32,14 @@ const express = require("express");
 
 // create application object
 const app = express();
-	
-	///////////////////////////////
-	// ROUTES
-	////////////////////////////////
-	// create a test route
-	app.get("/", (req, res) => {
-	    res.send("hello world");
-	});
+
+///////////////////////////////
+// ROUTES
+////////////////////////////////
+// create a test route
+app.get("/", (req, res) => {
+    res.send("hello world");
+});
 
 ///////////////////////////////
 // LISTENER
